535b9fe3f8e19029c1c243e032344d93125b7957
Billy <billyn0213@gmail.com>
2022-09-21 15:05:23 -0400

    moved secret key to .gitignore

--- dragonball_api/dragonball_api/settings.py
+++ dragonball_api/dragonball_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=l3cy-ju9_hgx@!_4(47v^%c7138t0)j&(i7(#4g=00*$u9*6b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
